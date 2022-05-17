2d4a46b91e7cbd04736636b41819884a457456c2
Thinhbp <90796376+Thinhbp@users.noreply.github.com>
2022-05-17 13:55:07 +0700

    Create token.sol

---
+++ token.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/blob/master/contracts/token/ERC20/ERC20Upgradeable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/blob/master/contracts/proxy/utils/Initializable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/blob/master/contracts/access/OwnableUpgradeable.sol";
+
+contract ABC is ERC20Upgradeable, OwnableUpgradeable {
+
+  function initialize() initializer public {
+    __ERC20_init("abc","ABC");
+    __Ownable_init();
+  }
+
+  function mint(uint256 amount) public onlyOwner {
+    _mint(msg.sender, amount*10**18);
+  }
+}
